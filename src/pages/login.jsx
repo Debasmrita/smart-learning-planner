49336387af254ea8e581bf821581ba95a89978b3
Debasmrita Banerjee <debasmrita2005@gmail.com>
2026-03-11 16:54:25 +0530

    commit

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -21,7 +21,7 @@ export default function Login() {
     try {
       setLoading(true);
 
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("https://smart-learning-planner-mbui.onrender.com/api/auth/profile", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
